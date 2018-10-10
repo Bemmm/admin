36183f2e22769fc0cf3b19f6c814c23d6f663f94
bohdan <bembenok@gmail.com>
2018-10-10 23:48:21 +0300

    build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ import env from '@env/.env';
 
 export const environment = {
   production: true,
-  version: env.npm_package_version,
-  serverUrl: 'https://api.chucknorris.io',
+  version: env.npm_package_version + '-dev',
+  serverUrl: '/api',
   defaultLanguage: 'en-US',
   supportedLanguages: [
     'en-US',
